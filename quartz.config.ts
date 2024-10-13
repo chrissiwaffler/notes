5d8e3677a37ea3329e9e77cba2cb5c4a808c3fe8
Christoph Waffler <waffler.christoph@gmail.com>
2024-10-14 00:54:47 +0200

    chore: update config

--- quartz.config.ts
+++ quartz.config.ts
@@ -8,15 +8,15 @@ import * as Plugin from "./quartz/plugins"
  */
 const config: QuartzConfig = {
   configuration: {
-    pageTitle: "🪴 Quartz 4.0",
-    pageTitleSuffix: "",
+    pageTitle: "🪴 Notes by Chrissi",
+    pageTitleSuffix: " | 🪴 Notes by Chrissi",
     enableSPA: true,
     enablePopovers: true,
     analytics: {
       provider: "plausible",
     },
     locale: "en-US",
-    baseUrl: "quartz.jzhao.xyz",
+    baseUrl: "notes.chrissi.xyz",
     ignorePatterns: ["private", "templates", ".obsidian"],
     defaultDateType: "created",
     theme: {
